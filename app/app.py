0b30cd23dafd7685a873dfa95ffd76c2c8b30fd4
Serdar Lyon <lyonserdar@gmail.com>
2020-03-05 19:02:53 -0700

    Trying to fix the run.py integration +++

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
-from services.security import authenticate, identity
 from resources.user import UserRegisterAPI
 from resources.item import ItemAPI, ItemListAPI
 from resources.store import StoreAPI, StoreListAPI
+
+from services.security import authenticate, identity
 
 app = Flask(__name__)
 app.debug = True
@@ -23,4 +24,14 @@ api.add_resource(ItemListAPI, "/items")
 api.add_resource(UserRegisterAPI, "/register")
 
 if __name__ == "__main__":
+    from services.database import db
+
+    db.init_app(app)
+
+    if app.config["DEBUG"]:
+
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
     app.run(port=5000, debug=True)
